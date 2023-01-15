cd4c655c8555c79d15968133e60039a5e2d3da9c
Partha Chakraborty <partha.c1999@gmail.com>
2023-01-15 17:37:47 +0530

    Added features in cart component adding and removing

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,8 +6,12 @@ import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
 function Cart(props) {
     const cartCtx=useContext(CartContext);
-    const cartItemRemoveHandler=id=>{};
-    const cartItemAddHandler=item=>{};
+    const cartItemRemoveHandler=id=>{
+        cartCtx.removeItem(id);
+    };
+    const cartItemAddHandler=item=>{
+        cartCtx.addItem({...item,amount:1});
+    };
     const totalAmount=`$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems=cartCtx.items.length>0;
     const cartItems=<ul className={classes['cart-items']}>{cartCtx.items.map((item)=>(
